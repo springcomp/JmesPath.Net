113af20f691faa65346edcae5a1ed00c0a99398a
Maxime Labelle <springcomp@users.noreply.github.com>
2020-11-01 08:52:11 +0100

    Renamed jmespath.net.tests folder and assembly for consistency purposes.

---
+++ tests/jmespath.net.tests/Blocks/BlockTest.cs
@@ -0,0 +1,13 @@
+﻿namespace jmespath.net.tests.Parser
+{
+    using FactAttribute = Xunit.FactAttribute;
+
+    public class BlockTest
+    {
+        [Fact]
+        public void ParseEmptyBlock()
+        {
+            const string expression = "{% %} foo";
+        }
+    }
+}

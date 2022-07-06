57eb3bdb3819fb8e328897c53f23d786dac55715
Maxime Labelle <springcomp@users.noreply.github.com>
2022-07-06 21:23:16 +0200

    [parser] custom impl of Gratt parser that supports lookhead tokens.

---
+++ src/jmespath.parser/Gratt/Public.cs
@@ -0,0 +1,6 @@
+﻿namespace Gratt
+{
+    public partial interface ILexer<TKind, TToken> {}
+    public partial class Parser {}
+    public partial class Parser<TState, TKind, TToken, TPrecedence, TResult> {}
+}

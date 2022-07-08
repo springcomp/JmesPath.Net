f26bb99d0b6b5f733fb0be07b9ca47f512be755f
springcomp <springcomp@users.noreply.github.com>
2022-07-08 19:33:44 +0200

    Compiles netcoreapp3.1 and net6.0 assemblies for performance comparisons.

--- tools/jmespathnet.compliance/ComplianceSummary.cs
+++ tools/jmespathnet.compliance/ComplianceSummary.cs
@@ -1,4 +1,22 @@
 ﻿namespace jmespath.net.compliance
 {
+#if NET6_0
     public record ComplianceSummary(double Percent, int Succeeded, int Total, int Failed);
+#else
+    public sealed class ComplianceSummary
+    {
+        public ComplianceSummary(double percent, int succeeded, int total, int failed)
+        {
+            Percent = percent;
+            Succeeded = succeeded;
+            Total = total;
+            Failed = failed;
+        }
+
+        public double Percent { get; }
+        public int Succeeded { get; }
+        public int Total { get; }
+        public int Failed { get; }
+    }
+#endif
 }

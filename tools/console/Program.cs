588b3bc4f5ba411829b91d13f0519c837840d728
Maxime Labelle <springcomp@users.noreply.github.com>
2022-07-11 20:17:39 +0200

    Newtonsoft is more efficient that System.Text.Json !

---
+++ tools/console/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using DevLab.JmesPath.Expressions;
+using DevLab.JmesPath.Utils;
+using JmesPath.Net.Expressions;
+using JmesPath.Net.Utils;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.Text.Json.Nodes;
+
+var jj = new JmesPath.Net.JmesPath();
+var jo = new DevLab.JmesPath.JmesPath();
+
+const string json = "{\"foo\": \"bar\"}";
+var nsj = JToken.Parse(json);
+var stj = JsonNode.Parse(json);
+
+const int COUNT = 100000;
+
+var identifier = new Identifier("foo");
+var sw = Stopwatch.StartNew();
+for (var index = 0; index < COUNT; index++)
+    identifier.Transform(stj);
+sw.Stop();
+Console.WriteLine($"{COUNT} iterations completed in {sw.Elapsed.TotalMilliseconds}ms.");
+
+var jid = new JmesPathIdentifier("foo");
+sw = Stopwatch.StartNew();
+for (var index = 0; index < COUNT; index++)
+    jid.Transform(nsj);
+sw.Stop();
+Console.WriteLine($"{COUNT} iterations completed in {sw.Elapsed.TotalMilliseconds}ms.");

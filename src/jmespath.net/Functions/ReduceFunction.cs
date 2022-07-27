79caed18c9a5d9573473c6952e71541e8e4c26d1
springcomp <springcomp@users.noreply.github.com>
2022-07-27 15:27:47 +0200

    [functions] `reduce` function.

---
+++ src/jmespath.net/Functions/ReduceFunction.cs
@@ -0,0 +1,70 @@
+﻿using DevLab.JmesPath.Expressions;
+using DevLab.JmesPath.Interop;
+using DevLab.JmesPath.Utils;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace DevLab.JmesPath.Functions
+{
+    public sealed class ReduceFunction : JmesPathFunction
+    {
+        public ReduceFunction(IScopeParticipant scopes)
+            : base("reduce", 3, scopes)
+        {
+        }
+
+        public override void Validate(params JmesPathFunctionArgument[] args)
+        {
+            System.Diagnostics.Debug.Assert(base.Scopes != null);
+
+            EnsureArray(args[0]);
+            EnsureExpressionType(args[2]);
+
+            if (args[1].IsExpressionType)
+                throw new Exception($"Error: invalid-type, unexpected expression type as the second parameter to the function {Name}.");
+
+            if (!(args[2].Expression is JmesPathLambdaExpression lambda))
+                throw new Exception($"Error: invalid-type, function {Name} expected a lambda-expression as its third parameter.");
+
+            if (lambda.Arguments.Length != 2)
+                throw new Exception($"Error: syntax, function {Name} expected a lambda-expression with exactly two arguments as its third parameters.");
+        }
+
+        public override JToken Execute(params JmesPathFunctionArgument[] args)
+        {
+            var acc = args[1].Token;
+            var array = (JArray)args[0].Token;
+
+            if (array.Count == 0)
+                return acc;
+
+            var lambda = args[2].Expression as JmesPathLambdaExpression;
+
+            foreach (var cur in array)
+            {
+                try
+                {
+                    // { acc: acc, cur: cur }
+
+                    var scope = new JObject();
+                    scope.Add(lambda.Arguments[0], acc);
+                    scope.Add(lambda.Arguments[1], cur);
+
+                    scopes_?.PushScope(scope);
+
+                    // evaluate lambda expression
+
+                    var result = lambda.Transform(JTokens.Null);
+
+                    // accumulate result
+
+                    acc = result.Token;
+                }
+                finally {
+                    scopes_?.PopScope();
+                }
+            }
+            return acc;
+        }
+    }
+}
---
+++ tests/jmespathnet.tests/Functions/ReduceFunctionTest.cs
@@ -0,0 +1,30 @@
+﻿using DevLab.JmesPath;
+using DevLab.JmesPath.Functions;
+using DevLab.JmesPath.Utils;
+using jmespath.net.tests.Parser;
+using Xunit;
+
+namespace jmespathnet.tests.Functions
+{
+    using FactAttribute = Xunit.FactAttribute;
+
+    public class ReduceFunctionTest : ParserTestBase
+    {
+        protected override void RegisterFunction(JmesPath parser)
+        {
+            parser
+                .FunctionRepository
+                .Register<ReduceFunction>();
+
+            base.RegisterFunction(parser);
+        }
+
+        [Theory]
+        [InlineData("reduce(@, `false`, (acc, cur) => acc || cur)", "[true, false, false]", "true")] // any
+        [InlineData("reduce(@, `false`, (acc, cur) => acc || cur)", "[false, false, false]", "false")] // any
+        [InlineData("reduce(@, `true`, (acc, cur) => acc && cur)", "[true, false, false]", "false")] // all
+        [InlineData("reduce(@, `true`, (acc, cur) => acc && cur)", "[true, true, true]", "true")] // all
+        public void JmesPathReduceFunctionTests(string expression, string json, string expected)
+            => Assert(expression, json, expected);
+    }
+}

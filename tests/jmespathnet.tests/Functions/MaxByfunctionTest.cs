217afb8b990094dcf6be426a569a4c0353e30bdc
Maxime Labelle <springcomp@users.noreply.github.com>
2024-04-11 19:51:27 +0200

    [max_by] invalid_type

---
+++ tests/jmespathnet.tests/Functions/MaxByfunctionTest.cs
@@ -0,0 +1,21 @@
+﻿using jmespath.net.tests.Parser;
+
+namespace jmespath.net.tests.Functions
+{
+    using FactAttribute = Xunit.FactAttribute;
+
+    public class MaxByfunctionTest : ParserTestBase
+    {
+        [Fact]
+        public void MaxBy()
+        {
+            const string expected = @"{""match_number"":1,""alliances"":{""red"":{""score"":120},""blue"":{""score"":60}}}";
+
+            string json = $"{{\"matches\": [{expected}]}}";
+
+            const string expression = "max_by(matches, &alliances.red.score)";
+
+            Assert(expression, json, expected);
+        }
+    }
+}
